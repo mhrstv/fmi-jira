003f415954423d208f4f00d9b02f1c093adf8e42
mhrstv <mhrstvv@gmail.com>
2026-06-04 00:12:44 +0300

    feat: add Comment class

--- src/models/comment/Comment.cpp
+++ src/models/comment/Comment.cpp
@@ -1,1 +1,38 @@
 #include "Comment.h"
+#include "../../users/User.h"
+#include "../../exceptions/ValidationException.h"
+
+Comment::Comment(const User* author, const std::string& content, const Date& date) : author(author), content(content), creationDate(date)
+{
+	if (!author)
+	{
+		throw ValidationException("Comment author cannot be null.");
+	}
+	if (content.empty())
+	{
+		throw ValidationException("Comment content cannot be empty.");
+	}
+}
+
+const User* Comment::getAuthor() const
+{
+	return author;
+}
+
+const std::string& Comment::getContent() const
+{
+	return content;
+}
+
+const Date& Comment::getCreationDate() const
+{
+	return creationDate;
+}
+
+std::ostream& operator<<(std::ostream& os, const Comment& comment)
+{
+	os << "[" << comment.creationDate.toString() << "] "
+		<< comment.author->getUsername() << ": "
+		<< comment.content;
+	return os;
+}
--- src/models/comment/Comment.h
+++ src/models/comment/Comment.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include "../../utils/Date.h"
+
+class User;
+
 class Comment
 {
+	const User* author;
+	std::string content;
+	Date creationDate;
 
+public:
+	Comment(const User* author, const std::string& content, const Date& date);
+
+	const User* getAuthor() const;
+	const std::string& getContent() const;
+	const Date& getCreationDate() const;
+
+	friend std::ostream& operator<<(std::ostream& os, const Comment& comment);
 };
